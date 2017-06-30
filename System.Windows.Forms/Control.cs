afd7e6bf36761d2b34df46b08104d41fa6c55338
samuel grahame <samuel@intelogy.com.au>
2017-06-30 12:24:39 +1000

    Added Events to Control

--- System.Windows.Forms/Control.cs
+++ System.Windows.Forms/Control.cs
@@ -225,5 +225,148 @@ namespace System.Windows.Forms
         private int x;
         private int y;
 
+        // Events: #TODO: Create other Event Handler Types
+        public event EventHandler AutoSizeChanged;
+        
+        public event EventHandler BackColorChanged;
+        
+        public event EventHandler BackgroundImageChanged;
+        
+        public event EventHandler BackgroundImageLayoutChanged;
+        
+        public event EventHandler BindingContextChanged;
+        
+        public event EventHandler CausesValidationChanged;
+        
+        public event UICuesEventHandler ChangeUICues;
+        
+        public event EventHandler Click;
+        
+        public event EventHandler ClientSizeChanged;
+        
+        public event EventHandler ContextMenuChanged;
+        
+        public event EventHandler ContextMenuStripChanged;
+        
+        public event ControlEventHandler ControlAdded;
+        
+        public event ControlEventHandler ControlRemoved;
+        
+        public event EventHandler CursorChanged;
+        
+        public event EventHandler DockChanged;
+        
+        public event EventHandler DoubleClick;
+        
+        public event EventHandler DpiChangedAfterParent;
+        
+        public event EventHandler DpiChangedBeforeParent;
+        
+        public event DragEventHandler DragDrop;
+        
+        public event DragEventHandler DragEnter;
+        
+        public event EventHandler DragLeave;
+        
+        public event DragEventHandler DragOver;
+        
+        public event EventHandler EnabledChanged;
+        
+        public event EventHandler Enter;
+        
+        public event EventHandler FontChanged;
+        
+        public event EventHandler ForeColorChanged;
+        
+        public event GiveFeedbackEventHandler GiveFeedback;
+        
+        public event EventHandler GotFocus;
+        
+        public event EventHandler HandleCreated;
+        
+        public event EventHandler HandleDestroyed;
+        
+        public event HelpEventHandler HelpRequested;
+        
+        public event EventHandler ImeModeChanged;
+        
+        public event InvalidateEventHandler Invalidated;
+        
+        public event KeyEventHandler KeyDown;
+        
+        public event KeyPressEventHandler KeyPress;
+        
+        public event KeyEventHandler KeyUp;
+        
+        public event LayoutEventHandler Layout;
+        
+        public event EventHandler Leave;
+        
+        public event EventHandler LocationChanged;
+        
+        public event EventHandler LostFocus;
+        
+        public event EventHandler MarginChanged;
+        
+        public event EventHandler MouseCaptureChanged;
+        
+        public event MouseEventHandler MouseClick;
+        
+        public event MouseEventHandler MouseDoubleClick;
+        
+        public event MouseEventHandler MouseDown;
+        
+        public event EventHandler MouseEnter;
+        
+        public event EventHandler MouseHover;
+        
+        public event EventHandler MouseLeave;
+        
+        public event MouseEventHandler MouseMove;
+        
+        public event MouseEventHandler MouseUp;
+        
+        public event MouseEventHandler MouseWheel;
+        
+        public event EventHandler Move;
+        
+        public event EventHandler PaddingChanged;
+        
+        public event PaintEventHandler Paint;
+        
+        public event EventHandler ParentChanged;
+        
+        public event PreviewKeyDownEventHandler PreviewKeyDown;
+        
+        public event QueryAccessibilityHelpEventHandler QueryAccessibilityHelp;
+        
+        public event QueryContinueDragEventHandler QueryContinueDrag;
+        
+        public event EventHandler RegionChanged;
+        
+        public event EventHandler Resize;
+        
+        public event EventHandler RightToLeftChanged;
+        
+        public event EventHandler SizeChanged;
+        
+        public event EventHandler StyleChanged;
+        
+        public event EventHandler SystemColorsChanged;
+        
+        public event EventHandler TabIndexChanged;
+        
+        public event EventHandler TabStopChanged;
+        
+        public event EventHandler TextChanged;
+        
+        public event EventHandler Validated;
+        
+        public event CancelEventHandler Validating;
+        
+        public event EventHandler VisibleChanged;
+
+
+
     }
 }
